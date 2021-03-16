5011d4d05eb2c5c5910dae0bc38003d0df1aa1f9
Mineco1006 <plakolm1006@gmail.com>
2021-03-16 23:23:02 +0100

    Update SmallStakeContract.sol

--- contracts/SmallStakeContract.sol
+++ contracts/SmallStakeContract.sol
@@ -1,57 +1,67 @@
 pragma solidity >0.4.99 <0.6.0;
 
-import './RootChainPoSWStaking.sol'
+import './RootChainPoSWStaking.sol';
 
 contract SmallStakeContract {
     address private signer;
-    address private constant stakingContract = 0x514b430000000000000000000000000000000001;
-    address public constant miner = ;
-    address public constant owner = ;
+    address payable private constant stakingContract = 0x514b430000000000000000000000000000000001;
+    address payable public constant miner = 0x0;
+    address payable public constant owner = 0x0;
 
     address[] user;
 
-    uint14 public minerFee;
+    uint16 public minerFee;
     uint256 public minerRewards;
+
+    uint16 public ownerFee;
 
     bool isClosed;
 
     uint256 public poolStakes;
-    mapping(address => uint12) private userWeight;
     mapping(address => uint256) public userStakes;
     mapping(address => uint256) public userRewards;
+
+    uint256 public withdrawableTimeStamp;
     
 
     RootChainPoSWStaking StakingContract = RootChainPoSWStaking(stakingContract);
 
-    constructor() {
+    constructor() public {
         signer = address(this);
         StakingContract.setSigner(signer);
     }
 
-    receive() external payable {
-        if(msg.sender == signer){
-            uint256 adjustedValue = msg.value - (msg.value*minerFee/10000)
-            for(uint16 i, i < user.length, i++) {
-                userRewards[user[i]] += (userWeight[user[i]]*adjustedValue)/10**18
+    function calculateRewards() internal {
+        uint256 _minerFee = msg.value*minerFee/10000;
+        uint256 _ownerFee = msg.value*ownerFee/10000;
+            miner.transfer(_minerFee);
+            owner.transfer(_ownerFee);
+
+            uint256 adjustedValue = msg.value - _minerFee;
+
+            for(uint16 i; i < user.length; i++) {
+                userRewards[user[i]] += ((userRewards[msg.sender]/poolStakes)*adjustedValue)/10**18;
             }
+    }
+
+    function () external payable {
+        if(msg.sender == signer){
+            calculateRewards();
         } else {
-            require(isClosed = false, 'Pool is already closed')
-            require(poolStakes + msg.value <= 1000100*10**18, 'Pool stakes too high')
+            require(isClosed = true, 'Pool is closed');
+
+            if(userStakes[msg.sender] == 0) {
+                user.push(msg.sender);
+            }
 
             userStakes[msg.sender] += msg.value;
             poolStakes += msg.value;
-
-            if(poolStakes >= 1000000*10**18) {
-                for(uint16 i, i <= user.lenght, i++) {
-                    userWeight[user[i]] = userStakes[user[i]]*10**18/poolStakes;
-                }
-                isClosed = true;
-            }
         }
     }
 
     function unlock() public {
         StakingContract.unlock();
+        withdrawableTimeStamp = block.timestamp + 3 days;
     }
     
     function lock() public {
@@ -63,27 +73,30 @@ contract SmallStakeContract {
         stakingContract.transfer(amount);
     }
 
-    function getWithdrawableTime() public view returns(uint256) {
-        return StakingContract.Stake.withdrawableTimestamp;
-    }
-
     function withdrawStakesFromStakingContract(uint256 amount) public {
         require(userStakes[msg.sender] >= amount);
         StakingContract.withdraw(amount);
     }
 
     function withdrawAllStakesFromStakingContract() public {
-        require(msg.sender == owner)
+        require(msg.sender == owner);
         StakingContract.withdrawAll;
     }
 
-    function adjustMinerFee(uint14 amount) {
-        require(msg.sender == owner || msg.sender == miner)
+    function adjustMinerFee(uint16 amount) public {
+        require(msg.sender == owner || msg.sender == miner);
+        require(minerFee <= 4000);
         minerFee = amount;
     }
 
-    function openPool() public {
+    function changePoolStatus(bool status) public {
         require(msg.sender == owner);
-        isClosed = false;
+        isClosed = status;
+    }
+
+    function adjustOwnerFee(uint16 amount) public {
+        require(msg.sender == owner);
+        require(amount <= 500);
+        ownerFee = amount;
     }
 }
